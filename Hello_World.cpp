99e8e3261453c736962640344bb501c443ef6f35
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-15 09:41:12 +0530

    Added other ways to write

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,5 +3,12 @@ using namespace std;
 int main()
 {
 cout << "Hello UTKARSH RAJ" << endl;
+
+cout << "Hello World !\n" ;
+
+cout << "Hello Earth" << "\n" ;
+
+cout << "My name is " <<"UTKARSH RAJ" ;
+
 return 0;
 }
