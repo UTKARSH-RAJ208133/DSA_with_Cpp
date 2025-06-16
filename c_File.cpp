211d8a31e79ba1322602ae9e26d3093618b44932
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-16 21:20:23 +0530

    Data Types in C++

---
+++ c_File.cpp
@@ -0,0 +1,18 @@
+//This is my first omment in C++
+// Press Ctrl + / (The selectrd code wil be comment-out)
+
+// Data Types
+// integer
+// float
+// character
+// bollian
+// Double
+
+#include <iostream>
+int main()
+{
+int age=20;
+std::cout << "Your age is :" << age ;
+// std::cout << "Your age is :" << age << endl ; is having a problem ?
+
+}
