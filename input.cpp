17fedd6b7d6cb43644739e6f0e9b8dd153cfd371
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-21 22:56:27 +0530

    Taking input in C++

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//Learnng how to take input in C++
+int age; // A memory location got alloted named age with garbege value stored i.e. random any value got stored
+cout <<age <<"\n";
+
+cout <<"Inter our Age :";
+cin >>age;
+cout <<"You entered your age as : " <<age << "\n";
+
+char confermation;
+cout <<"Press Y if your entered age is correct else Press N :";
+cin >> confermation;
+
+return 0 ;
+}
