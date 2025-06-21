cfc769da51985c1ff370dbaca22c13536d2ba180
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-21 21:43:42 +0530

    Explicit type casting

--- variables.cpp
+++ variables.cpp
@@ -3,13 +3,19 @@ using namespace std;
 int main()
 {
 //Type Casting
+//1. Implicit (known as Conversion)
 char alpha='A';
 int beta=alpha;
-cout <<beta <<"\n";
-//above line code will return the ASCII value of character
+cout <<beta <<"\n"; //this line code will return the ASCII value of character
+
 char gamma='B';
 int delta=gamma;
 cout <<delta <<endl;
+
+//2. Explicit (known as Casing)
+double theeta=166.66;
+int omega=(int)theeta; //we tell new data type in the parrenthesis before the value to be type casted
+cout << omega <<endl;
 
 return 0;
 }
