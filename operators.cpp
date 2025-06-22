483189c4f7bde358cf4d098d98a4124ad1048a78
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-22 13:31:35 +0530

    Operating the arithmatic operators

---
+++ operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a=10, b=15; //We can define multiple variables in same line just by separating by comma
+
+int gamma=a+b ;
+cout << "Sun is : " <<gamma <<endl;
+cout <<"Sum is : " << (a+b) <<endl; //Alternate code to above 2 line of code
+cout <<"Product is : " <<(a*b) <<endl;
+
+cout <<"Difference is : " <<(a-b) <<" or " <<(b-a) <<endl;
+cout <<"Quotient is : " <<(a/b) <<" or " <<(b/a) <<endl;
+cout <<"The remainder upon devision of b with a is : " <<(b%a) <<endl;
+
+cout <<(5/(double)2) <<endl; //Writing the data type in parrenthisis before data, will type caste the data to data type mentioned in parrenthisis
+
+
+    return 0;
+}
