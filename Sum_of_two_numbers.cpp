aefab6288f01838a6081c525ebc2bdd4e6b3e4ac
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-22 20:55:28 +0530

    A program to find the sum of two numbers given by the user

---
+++ Sum_of_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float num1 , num2 ;
+cout <<"Enter first number : ";
+cin >> num1;
+cout << "Enter second number : ";
+cin >> num2;
+cout << "The SUM of given two numbers is : " <<(num1+num2) <<endl;
+
+    return 0;
+}
