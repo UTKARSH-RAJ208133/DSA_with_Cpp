312d844deaeb8ee673fb0b8e4e1c082fe7ab77e8
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-06-23 16:47:27 +0530

    Code for square pattern of type 1

---
+++ Patterns/square_pattern.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout <<"Enter the number of sides you want for a Square : " ;
+    cin >>n;
+    cout <<endl ;
+
+// Square formed by numbers
+cout <<"Square formed by numbers" <<endl;
+
+    for (int i=1; i<=n;i++) {   //Outer loop for changing the line
+
+        for(int j=1; j<=n; j++){    //Inner loop for data in each line
+            cout <<j;
+            cout <<" ";         //For space between numers
+        }
+
+        cout <<endl;        //Endline Character so that after every alteration next line is used
+    }
+
+
+
+
+// Square formed by Stars
+cout <<"Square formed by Stars" <<endl;
+
+    for (int i=1; i<=n;i++) {   //Outer loop for changing the line
+
+        for(int j=1; j<=n; j++){    //Inner loop for data in each line
+            cout <<"* ";
+        }
+
+        cout <<endl;        //Endline Character so that after every alteration next line is used
+    }
+
+
+
+
+// Square of Characters
+cout <<"Square formed by Characters" <<endl;
+
+   for (int i=1; i<=n;i++) {   //Outer loop for changing the line
+        char ch='A' ;
+        for(int j=1; j<=n; j++){    //Inner loop for data in each line
+            cout <<ch <<" ";
+            ch=ch+1 ;
+        }
+
+        cout <<endl;        //Endline Character so that after every alteration next line is used
+    }
+    
+    return 0;
+}
