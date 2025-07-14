2e199313c6c46bec1ac6899e174e518658ed0c8a
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-07-14 22:29:14 +0530

    difference between standard if-else vs ternary if-else

--- ternary_statement.cpp
+++ ternary_statement.cpp
@@ -6,6 +6,16 @@ int main()
     float n;
     cout << "Enter the number : ";
     cin >> n;
-    cout << (n>=0 ? (n==0 ? "Zero" : "Positive"):"Negative" );
+    cout << (n>=0 ? (n==0 ? "Zero" : "Positive"):"Negative" ); // two if else used as single ternary statement
     return 0;
+
+    // if (n>=0){
+    //     if (n==0){
+    //         cout << "ZERO";
+    //     }else {
+    //         cout << "Positive";
+    //     }
+    // }else {
+    //     cout << "Negative";
+    // }
 }
