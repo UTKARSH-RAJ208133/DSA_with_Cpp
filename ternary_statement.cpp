84bc8b93caf2fbc3ff74e671ca15df7c2b636cd7
UTKARSH-RAJ208133 <utkarsh.raj.208133@gmail.com>
2025-07-14 21:35:23 +0530

    ternary statements

---
+++ ternary_statement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float n;
+    cout << "Enter the number : ";
+    cin >> n;
+    cout << (n>=0 ? "Non-ngative":"Negative" );
+    return 0;
+}
